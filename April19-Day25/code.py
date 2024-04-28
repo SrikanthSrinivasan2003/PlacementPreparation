6da8f287379efa21dd7ba2800862ba5bd2ebfa8d
SrikanthSrinivasan2003 <154430800+SrikanthSrinivasan2003@users.noreply.github.com>
2024-04-28 21:47:59 +0530

    Create code.py

---
+++ April19-Day25/code.py
@@ -0,0 +1,11 @@
+''' 1. Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+Example 1:
+Input: nums = [2,2,1]
+Output: 1 '''
+
+
+n=list(map(int,input().split()))
+for i in n:
+    if n.count(i)==1:
+        print(i)
